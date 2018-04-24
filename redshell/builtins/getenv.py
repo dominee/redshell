5d2d18512706fd17000258abf4f164db3fe92314
Dominee Fromhell <dominee@hell.sk>
2018-04-24 12:27:09 +0200

    added help and usage to getenv

--- redshell/builtins/getenv.py
+++ redshell/builtins/getenv.py
@@ -5,11 +5,27 @@ def getenv_usage():
     sys.stdout.write("getenv\n")
     return SHELL_STATUS_RUN
 
-def getenv(args):
-    # Do we need to display usage or help?
-    if args[0] == 'usage':
-        return getenv_usage()
+def getenv_help():
+    sys.stdout.write("""
+[getenv]
+Usage : getenv <variable>
 
+Get OS environment variable.
+
+Example:
+getenv PATH
+
+""")
+    return SHELL_STATUS_RUN
+
+def getenv(args):
     if len(args) > 0:
-        sys.stdout.write(os.getenv(args[0]))
+        # Do we need to display usage or help?
+        if args[0] == 'usage':
+            return getenv_usage()
+        if args[0] == 'help':
+            return getenv_help()
+        sys.stdout.write(os.getenv(args[0])+"\n")
+    else:
+        sys.stdout.write("Variable name required.\n")
     return SHELL_STATUS_RUN
