772fec3832797a63080bd71555cc1f73b2be12d9
Dominee Fromhell <dominee@hell.sk>
2018-05-29 21:34:27 +0200

    added external nameserver option

--- redshell/builtins/resolve.py
+++ redshell/builtins/resolve.py
@@ -9,7 +9,7 @@ from redshell.config import *
 def resolve_help():
     sys.stdout.write("""
 [resolve]
-Usage : resolve <domain>
+Usage : resolve <domain> [nameserver]
 
 Resolve a domain name recursively
 
@@ -19,7 +19,7 @@ resolve github.com
     return SHELL_STATUS_RUN
 
 def resolve_usage():
-    sys.stdout.write("resolve <domain>\n")
+    sys.stdout.write("resolve <domain> [nameserver]\n")
     return SHELL_STATUS_RUN
 
 def resolve(args):
@@ -30,36 +30,52 @@ def resolve(args):
             return resolve_help()
         if args[0] == 'usage':
             return resolve_usage()
+
+        # Create our own resolver instance
+        my_resolver = dns.resolver.Resolver()
+        my_resolver.timeout = 1
+        my_resolver.lifetime = 1
+        # Assign target domain/host
         h = args[0]
+        # If a resolver was specified, use it
+        if len(args) == 2:
+            my_resolver.nameservers = [args[1]]
+            # Check if the resolver is working
+            try:
+                my_resolver.query('google.com', 'A')[0]
+                sys.stdout.write("[+] Nameserver: "+args[1]+"\n")
+            except dns.exception.Timeout:
+                print 'Resolver timeout.'
+                return SHELL_STATUS_RUN
     else:
         resolve_usage()
 
     # Check for A and CNAME
     sys.stdout.write("[+] Resolving "+h+" \n")
-    for ip in dns.resolver.query(h, 'A'):
+    for ip in my_resolver.query(h, 'A'):
         try:
-            hostname = dns.resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
-            sys.stdout.write("[*] IP: "+str(ip)+" \t-> "+str(hostname)+"\n")
+            hostname = my_resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
+            sys.stdout.write("[*] IP: "+str(ip)+" -> "+str(hostname)+"\n")
         except:
             sys.stdout.write("[*] IP: "+str(ip)+"\n")
 
 
     # Get MX records, resolve to IP and PTR
     try:
-        for mx in dns.resolver.query(h, 'MX'):
+        for mx in my_resolver.query(h, 'MX'):
             try:
                 mx_host = str(mx).split(' ')[1]
-                ip = dns.resolver.query(mx_host, 'A')[0]
-                hostname = dns.resolver.query(dns.reversename.from_address(str(ip)),'PTR')[0]
-                sys.stdout.write("[ ] MX: "+str(mx)+" \t-> "+str(ip)+" \t-> "+str(hostname)+"\n")
+                ip = my_resolver.query(mx_host, 'A')[0]
+                hostname = my_resolver.query(dns.reversename.from_address(str(ip)),'PTR')[0]
+                sys.stdout.write("[ ] MX: "+str(mx)+" -> "+str(ip)+" -> "+str(hostname)+"\n")
             except:
-                sys.stdout.write("[ ] MX: "+str(mx)+" \t-> "+str(ip)+"\n")
+                sys.stdout.write("[ ] MX: "+str(mx)+" -> "+str(ip)+"\n")
     except:
         pass
     
     # Get NS records, resolve to IP and PTR
     try:
-        for ns in dns.resolver.query(h, 'NS'):
+        for ns in my_resolver.query(h, 'NS'):
             try:
                 # Try to make a AXFR zone transfer
                 z = dns.zone.from_xfr(dns.query.xfr(ns, h))
@@ -74,25 +90,25 @@ def resolve(args):
                 pass
             # If AXFR is not possible, just resolve NS records
             try:
-                ip = dns.resolver.query(str(ns), 'A')[0]
-                hostname = dns.resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
-                sys.stdout.write("[ ] NS: "+str(ns)+" \t-> "+str(ip)+" \t-> "+str(hostname)+"\n")
+                ip = my_resolver.query(str(ns), 'A')[0]
+                hostname = my_resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
+                sys.stdout.write("[ ] NS: "+str(ns)+" -> "+str(ip)+" -> "+str(hostname)+"\n")
             except:
-                sys.stdout.write("[ ] NS: "+str(ns)+" \t-> "+str(ip)+"\n")
+                sys.stdout.write("[ ] NS: "+str(ns)+" -> "+str(ip)+"\n")
                     
     except:
         pass
 
     # Check for AAAA records
     try:    
-        for aaaa in dns.resolver.query(h, 'AAAA'):
+        for aaaa in my_resolver.query(h, 'AAAA'):
             sys.stdout.write("[ ] AAAA: "+str(aaaa)+"\n")        
     except:
         pass
  
     # Check for TXT  records
     try:    
-        for txt in dns.resolver.query(h, 'TXT'):
+        for txt in my_resolver.query(h, 'TXT'):
             sys.stdout.write("[ ] TXT: "+str(txt)+"\n")        
     except:
         pass
