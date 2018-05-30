78bf233b055425d82a4c676413a5339e08cfbfb4
Dominee Fromhell <dominee@hell.sk>
2018-05-30 20:24:41 +0200

    use of line start constants

--- redshell/builtins/resolve.py
+++ redshell/builtins/resolve.py
@@ -56,7 +56,7 @@ def resolve(args):
             # Check if the resolver is working
             try:
                 my_resolver.query('google.com', 'A')[0]
-                sys.stdout.write("[+] Nameserver: "+args[1]+"\n")
+                sys.stdout.write(PLUS+"Nameserver: "+args[1]+"\n")
             except dns.exception.Timeout:
                 print 'Resolver timeout.'
                 return SHELL_STATUS_RUN
@@ -64,18 +64,18 @@ def resolve(args):
         resolve_usage()
 
     # Check for A
-    sys.stdout.write("[+] Resolving "+colored(h,'white')+" \n")
+    sys.stdout.write(PLUS+"Resolving "+colored(h,'white')+" \n")
     for ip in my_resolver.query(h, 'A'):
         try:
             hostname = my_resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
-            sys.stdout.write("[*] IP: "+colored(str(ip),'white')+" -> "+str(hostname)+"\n")
+            sys.stdout.write(STAR+"IP: "+colored(str(ip),'white')+" -> "+str(hostname)+"\n")
         except:
-            sys.stdout.write("[*] IP: "+colored(str(ip),'white')+"\n")
+            sys.stdout.write(STAR+"IP: "+colored(str(ip),'white')+"\n")
 
     # Check for CNAME
     try:
         cname = my_resolver.query(h, 'CNAME')[0]
-        sys.stdout.write("[*] "+h+" is an alias for "+colored(str(cname),'white')+"\n")
+        sys.stdout.write(STAR+""+h+" is an alias for "+colored(str(cname),'white')+"\n")
     except:
         pass
 
@@ -87,9 +87,9 @@ def resolve(args):
                 mx_host = str(mx).split(' ')[1]
                 ip = my_resolver.query(mx_host, 'A')[0]
                 hostname = my_resolver.query(dns.reversename.from_address(str(ip)),'PTR')[0]
-                sys.stdout.write("[ ] MX: "+str(mx)+" -> "+str(ip)+" -> "+str(hostname)+"\n")
+                sys.stdout.write(LS+"MX: "+str(mx)+" -> "+str(ip)+" -> "+str(hostname)+"\n")
             except:
-                sys.stdout.write("[ ] MX: "+str(mx)+" -> "+str(ip)+"\n")
+                sys.stdout.write(LS+"MX: "+str(mx)+" -> "+str(ip)+"\n")
     except:
         pass
     
@@ -112,9 +112,9 @@ def resolve(args):
             try:
                 ip = my_resolver.query(str(ns), 'A')[0]
                 hostname = my_resolver.query(dns.reversename.from_address(str(ip)), 'PTR')[0]
-                sys.stdout.write("[ ] NS: "+str(ns)+" -> "+str(ip)+" -> "+str(hostname))
+                sys.stdout.write(LS+"NS: "+str(ns)+" -> "+str(ip)+" -> "+str(hostname))
             except:
-                sys.stdout.write("[ ] NS: "+str(ns)+" -> "+str(ip))
+                sys.stdout.write(LS+"NS: "+str(ns)+" -> "+str(ip))
 
             try:
                 chaos = timeout_command([REDSHELL_DIG, '@'+str(ns), "version.bind", "txt", "chaos", "+short"], 1)
@@ -131,14 +131,14 @@ def resolve(args):
     # Check for AAAA records
     try:    
         for aaaa in my_resolver.query(h, 'AAAA'):
-            sys.stdout.write("[ ] AAAA: "+str(aaaa)+"\n")        
+            sys.stdout.write(LS+"AAAA: "+str(aaaa)+"\n")        
     except:
         pass
  
     # Check for TXT  records
     try:    
         for txt in my_resolver.query(h, 'TXT'):
-            sys.stdout.write("[ ] TXT: "+str(txt)+"\n")        
+            sys.stdout.write(LS+"TXT: "+str(txt)+"\n")        
     except:
         pass
  
