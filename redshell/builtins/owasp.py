0d28e35a38948b2b87f7c39be2e4ef89704ca6f5
Dominee Fromhell <dominee@hell.sk>
2018-06-18 14:50:23 +0200

    fixed response code

--- redshell/builtins/owasp.py
+++ redshell/builtins/owasp.py
@@ -65,35 +65,35 @@ def owasp(args):
     paths = ['/','/robots.txt','/crossdomain.xml','/clientaccesspolicy.xml']
     for path in paths:
         response = session.get(host + path, headers=headers, verify=False)
-        print(LS+"%i %s" % (colored(response.status_code,'white'),path))
+        print(LS+"%s %s" % (colored(response.status_code,'white'),path))
 
     # Request errors
     # 400 Bad Request - unknown method (WTF)
     response = session.request('WTF',host + '/', headers=headers, verify=False)
-    print(LS+"Bad Request (WTF) [400] : %i"  % colored(response.status_code,'white'))
+    print(LS+"Bad Request (WTF) [400] : %s"  % colored(response.status_code,'white'))
 
     # 405 Method Not Allowed
     response = session.request('MKCOL',host + '/', headers=headers, verify=False)
-    print(LS+"Method Not Allowed (MKCOL) [405] : %i"  % colored(response.status_code,'white'))
+    print(LS+"Method Not Allowed (MKCOL) [405] : %s"  % colored(response.status_code,'white'))
 
     # 414 URI Too Long
     paramsGet = {"very_long_param": "A"*10097}
     response = session.get(host + "/", params=paramsGet, headers=headers, verify=False)
-    print(LS+"URI Too Long [414] : %i"  % colored(response.status_code,'white'))
+    print(LS+"URI Too Long [414] : %s"  % colored(response.status_code,'white'))
 
     # 413 Payload Too Large
     rawBody = "A"*1024*1024*5
     response = session.post(host + "/", data=rawBody, headers=headers,verify=False)
-    print(LS+"Payload Too Large [413] : %i"  % colored(response.status_code,'white'))
+    print(LS+"Payload Too Large [413] : %s"  % colored(response.status_code,'white'))
 
     # 417 Expectation Failed
     response = session.get(host + '/', headers={"Accept-Encoding": "gzip, deflate", "Upgrade-Insecure-Requests": "1", "Accept-Language": "en-US,en;q=0.5", "User-Agent": REDSHELL_UA, "Connection": "close","Expect":"666-evil"}, verify=False)
-    print(LS+"Expectation Failed [417] : %i"  % colored(response.status_code,'white'))
+    print(LS+"Expectation Failed [417] : %s"  % colored(response.status_code,'white'))
 
 
     # 431 Request Header Fields Too Large
     response = session.get(host + '/', headers={"Accept-Encoding": "gzip, deflate", "Upgrade-Insecure-Requests": "1", "Accept-Language": "en-US,en;q=0.5", "User-Agent": 'A'*10097, "Connection": "close"}, verify=False)
-    print(LS+"Request Header Fields Too Large [431] : %i"  % colored(response.status_code,'white'))
+    print(LS+"Request Header Fields Too Large [431] : %s"  % colored(response.status_code,'white'))
 
     # 505 HTTP Version Not Supported
     vsn =  httplib.HTTPConnection._http_vsn
@@ -101,13 +101,13 @@ def owasp(args):
     httplib.HTTPConnection._http_vsn = 10
     httplib.HTTPConnection._http_vsn_str = 'HTTP/1.0'
     response = session.get(host + '/', headers=headers, verify=False)
-    print(LS+"HTTP Version Not Supported [505] : %i"  % colored(response.status_code,'white'))
+    print(LS+"HTTP Version Not Supported [505] : %s"  % colored(response.status_code,'white'))
     httplib.HTTPConnection._http_vsn = vsn
     httplib.HTTPConnection._http_vsn_str = vsn_str
 
     # 101 Switching Protocols
     response = session.get(host + '/', headers={"Accept-Encoding": "gzip, deflate", "Upgrade-Insecure-Requests": "1","Accept-Language": "en-US,en;q=0.5", "User-Agent": REDSHELL_UA, "Connection": "Upgrade", "Upgrade":"websocket"}, verify=False)
-    print(LS+"101 Switching Protocols (websocket) [101] : %i"  % colored(response.status_code,'white'))
+    print(LS+"101 Switching Protocols (websocket) [101] : %s"  % colored(response.status_code,'white'))
 
     return SHELL_STATUS_RUN
 
